b622d32a733196fa624b6a797b337f9d74a619bc
Jephthah Orobia <jephthah.orobia@gmail.com>
2024-10-05 03:21:32 -0500

    remove unnecessary comments

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,8 @@
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////
-//                               NOTE
-// This is your program entry point. Your main logic is placed
-// inside this function. You may add your functions before this
-// "main()", or after this "main()" provided you added reference
-// before this "main()" function.
-//////////////////////////////////////////////////////////////////
-
 int main()
 {
-  // ************************** TO DO **************************
-  // Place your code logic after this comment line
-  // ***********************************************************
-
   char name[50];
 
   const char* divider = "\n================================================\n";
