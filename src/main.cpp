2fadd67969897b76266c9c63fccf16b169a4e276
Jephthah Orobia <jephthah.orobia@gmail.com>
2024-10-05 03:11:44 -0500

    remove "first" in the question

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,12 @@
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////
-//                               NOTE
-// This is your program entry point. Your main logic is placed
-// inside this function. You may add your functions before this
-// "main()", or after this "main()" provided you added reference
-// before this "main()" function.
-//////////////////////////////////////////////////////////////////
-
 int main()
 {
-  // ************************** TO DO **************************
-  // Place your code logic after this comment line
-  // ***********************************************************
-
+ 
   char section[50];
 
-
-  cout << "What is your first section? ";
+  cout << "What is your section? ";
 
   cin >> section;
 
