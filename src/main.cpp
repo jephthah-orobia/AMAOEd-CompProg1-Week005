5ad51e5f9e10dabbfad38acb7dc21119cbac0c6b
Jephthah Orobia <jephthah.orobia@gmail.com>
2024-10-05 02:17:49 -0500

    Initial file from C++ Template

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/* ****************************************************************
+ * @author: JEPHTHAH M. OROBIA
+ * @app name: AMAOEd-Comp1-Week5 LabWorks
+ * @app desc: To complete task stated in LabortoryExercise004 of ITE7101
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main()
+{
+  // ************************** TO DO **************************
+  // Place your code logic after this comment line
+  // ***********************************************************
+
+  cout << "Hello World!" << endl;
+
+  // ********************** DO NOT CHANGE **********************
+  // Print a new line and ask user for any key before exiting
+  // ***********************************************************
+  _pause();
+  return EXIT_SUCCESS;
+}
